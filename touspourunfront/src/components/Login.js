f6163dafb2eca72cadbe44703034bc6e362d10d5
HanaAc22 <acdev22@outlook.fr>
2023-05-10 20:29:59 +0200

    changing error console.log

--- touspourunfront/src/components/Login.js
+++ touspourunfront/src/components/Login.js
@@ -39,7 +39,7 @@ export default function Login() {
 
         if (!response.ok) {
             setError(data.error);
-            console.log('error ❌');
+            console.log('error');
             return;
         }
 
