9dde203687ef92468f8885cdd83750eb460d86e9
HanaAc22 <acdev22@outlook.fr>
2023-05-11 22:36:57 +0200

    installing react with webpack

--- www/touspourun/src/Controller/SecurityController.php
+++ www/touspourun/src/Controller/SecurityController.php
@@ -13,23 +13,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class SecurityController extends AbstractController
 {
     #[Route('/login', name: 'app_login', methods: ['GET', 'POST'])]
-    public function login(Request $request, EntityManagerInterface $entityManager): Response
+    public function login(): Response
     {
-        $email = $request->request->get('email');
-        $password = $request->request->get('password');
-
-        // Query the database for the user with the given email
-        $user = $entityManager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-        // Check if the user exists and the password is correct
-        if (!$user || !password_verify($password, $user->getPassword())) {
-            return new Response('Invalid email or password', 401);
-        }
-
-        // Set the user in the session
-        $request->getSession()->set('user', $user);
-
-        return new Response('Authenticated', 200);
+      return $this->render('/security/index.html.twig');
     }
 }
 
